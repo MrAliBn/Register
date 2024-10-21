f04331fa8bac90e8c274f06ff088d3229c105d3e
MrAli <alibaneshi2gmail.com>
2024-10-21 21:47:34 +0330

    add directory be and pl

--- pl/index.py
+++ pl/index.py
@@ -1,26 +1,36 @@
-from tkinter import *
 from be.Register_User import Register
-
-win = Tk()
-
-# Setting Screen
-
-win.geometry('800x600')
-win.title("Register")
-win.resizable(width=False, height=False)
-
-class App:
-    def __init__(self):
-        pass
-
-# Text Screen
-
-win.config(bg='blue')
-txtnamee = StringVar()
-txtname = Entry(win, width=25, background="white", foreground="#000000", bd=3, justify='center', textvariable=self.name)
-txtname.place(x=200, y=100)
+from tkinter import messagebox, ttk
+from tkinter import *
 
 
-txtnamee.set("Name")
+class App(Frame):
+    def __init__(self, screen):
+        super().__init__(screen)
+        self.screen = screen
+        self.text_screen()
 
-win.mainloop()
+# screen text
+    def text_screen(self):
+        self.name = StringVar()
+        self.txtname = Entry(self.screen, textvariable=self.name)
+        self.txtname.configure(bg="white", fg="black", bd=3, justify="center")
+        self.txtname.place(x=300, y=50)
+        self.name.set("Username")
+
+        self.family = StringVar()
+        self.txtfamily = Entry(self.screen, textvariable=self.family)
+        self.txtfamily.configure(bg="white", fg="black", bd=3, justify="center")
+        self.txtfamily.place(x=300, y=100)
+        self.family.set("Family")
+
+        self.filde = StringVar()
+        self.txtfilde= Entry(self.screen, textvariable=self.filde)
+        self.txtfilde.configure(bg="white", fg="black", bd=3, justify="center")
+        self.txtfilde.place(x=300, y=150)
+        self.filde.set("Filde")
+
+        self.age = StringVar()
+        self.txtage = Entry(self.screen, textvariable=self.age)
+        self.txtage.configure(bg="white", fg="black", bd=3, justify="center")
+        self.txtage.place(x=300, y=200)
+        self.age.set("Username")
