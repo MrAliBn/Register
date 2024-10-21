5a33c8f80ae9c40b4e75d093fcf13bf5d89fcc66
MrAli <alibaneshi2gmail.com>
2024-10-21 23:03:05 +0330

    set messege box in oneclick

--- pl/index.py
+++ pl/index.py
@@ -11,8 +11,22 @@ class App(Frame):
 
     # function
     def OneClickedRegister(self):
-        user = Register_User(self.txtname.get(), self.txtfamily.get(), self.txtfilde.get(), self.txtage.get())
-        user.add_user()
+        if self.name.get() == "Username" or self.txtname.get() == '' or not self.txtname.get().isalpha():
+            messagebox.showerror("Error", "Please enter your name is alpha")
+            self.txtname.focus()
+        elif self.family.get() == "Family" or self.txtfamily.get() == '' or not self.txtfamily.get().isalpha():
+            messagebox.showerror("Error", "Please enter your family name is alpha")
+            self.txtfamily.focus()
+        elif self.filde.get() == "Filed" or self.txtfilde.get() == '' or not self.txtfilde.get().isalpha():
+            messagebox.showerror("Error", "Please enter your filed is alpha")
+            self.txtfilde.focus()
+        elif self.age.get() == "Year" or self.txtage.get() == '' or not self.txtage.get().isdigit():
+            messagebox.showerror("Error", "Please enter your age is digit")
+            self.txtage.focus()
+        else:
+            user = Register_User(self.txtname.get(), self.txtfamily.get(), self.txtfilde.get(), self.txtage.get())
+            user.add_user()
+            messagebox.showinfo("Registered", "Registered")
 
 # screen text
     def text_screen(self):
@@ -32,13 +46,13 @@ class App(Frame):
         self.txtfilde= Entry(self.screen, textvariable=self.filde)
         self.txtfilde.configure(bg="white", fg="black", bd=3, justify="center")
         self.txtfilde.place(x=300, y=150)
-        self.filde.set("Filde")
+        self.filde.set("Filed")
 
         self.age = StringVar()
         self.txtage = Entry(self.screen, textvariable=self.age)
         self.txtage.configure(bg="white", fg="black", bd=3, justify="center")
         self.txtage.place(x=300, y=200)
-        self.age.set("Age")
+        self.age.set("Year")
 
         # Button Screen
 
